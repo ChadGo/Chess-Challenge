3b15afd871f870187ab022d6fc6752803d2ce4b9
Chad Golob <chad.golob@gmail.com>
2023-08-30 22:26:20 -0700

    Add my bot with transposition table

--- Chess-Challenge-Tests/BoardEvalTest.cs
+++ Chess-Challenge-Tests/BoardEvalTest.cs
@@ -1,4 +1,4 @@
-﻿using ChessChallenge.Chess;
+using ChessChallenge.Chess;
 
 public class BoardEvalTest
 {
@@ -53,6 +53,15 @@ public class BoardEvalTest
     }
 
     [Fact]
+    public void BlackKingShortSideEval()
+    {
+        var evaluation = GetEvaluation("6k1/8/8/8/8/8/8/8 w - - 0 1");
+
+        Assert.Equal(-10024, evaluation);
+    }
+
+    
+    [Fact]
     public void OnlyShortSideCastledRookAndKing()
     {
         var evaluation = GetEvaluation("5rk1/8/8/8/8/8/8/5RK1 w - - 0 1");
@@ -65,12 +74,56 @@ public class BoardEvalTest
     {
         for (int index = 0; index < 64; index++)
         {
-            var lookup_index = ((63 - index) - ((63 - index) % 8) + (index % 8));
-            Console.WriteLine($"Index: {index}, black index: {lookup_index}");
+            //var the_worst_lookup_index = ((63 - index) - ((63 - index) % 8) + (index % 8));
+            var the_worst_lookup_index = 63 - index;
+            Console.WriteLine($"Index: {index}, black index: {the_worst_lookup_index}");
+
+
+
+
+            var index_row_number = index / 8;
+            var index_column_number = index % 8;
+
+            var lookup_row_number = true ? 7 - index_row_number : index_row_number;
+            var lookup_column_number = index_column_number;
+
+            var the_best_lookup_index = lookup_row_number * 8 + lookup_column_number;
+
+
+            Assert.Equal(the_best_lookup_index, the_worst_lookup_index);
         }
         
 
     }
+
+
+    [Fact]
+    public void E4Response()
+    {
+        var e4_e5_eval = GetEvaluation("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2");
+        var e4_a6_eval = GetEvaluation("rnbqkbnr/1ppppppp/p7/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2");
+
+        Console.WriteLine(e4_e5_eval);
+        Console.WriteLine(e4_a6_eval);
+
+        Assert.True(e4_e5_eval > e4_a6_eval);
+    }
+
+
+    [Fact]
+    public void E4E5Response()
+    {
+        var e4_e5_eval = GetEvaluation("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2");
+        Assert.Equal(0, e4_e5_eval);
+    }
+
+    [Fact]
+    public void E4A5Response()
+    {
+        var e4_a6_eval = GetEvaluation("rnbqkbnr/1ppppppp/p7/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2");
+        Assert.Equal(0, e4_a6_eval);
+    }
+
 
     private int GetEvaluation(string fen)
     {
